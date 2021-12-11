6d779d561c8af196a3511c98ac239e3a33ddd248
oznerm <renzomaldonado@gmail.com>
2021-12-10 20:00:16 -0500

    minor

--- St_blog.py
+++ St_blog.py
@@ -9,3 +9,12 @@ st.title("Sooner or later?  Walkthrough to predict when an elite soccer player w
 st.write("Sravan Rajupalem") 
 st.write("Renzo Maldonado")
 st.write("Victor Ruiz")
+
+section = st.sidebar.selectbox("Sections", ("Scraping the Web for Data", "Data Manipulation", "Feature Engineering", 
+    "Visual Exploration of Data", "Model Building"))
+
+st.write("""For quite a while, 'Sports Analytics' has been the buzz-word in the world of Data Science. Magically using complex 
+    algorithms, machine learning models and neural networks to predict sports results and players' performance attract the interest 
+    of people for different reasons. Soccer is probably one of the most unpredictable sports out there. In the hope of aiding soccer 
+    managers' decisions, we decided to apply Data Science tools to predict how likely a player was to have an injury within a 
+    certain time frame.""")
