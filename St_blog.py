4720466df74298d72d293593222fc0bf8d6a103a
dsvictorruiz <dsvictor@umich.edu>
2021-12-18 13:55:54 -0500

    Merge branch 'main' of https://github.com/SravanRajupalem/FootballPredictions

--- St_blog.py
+++ St_blog.py
@@ -496,3 +496,41 @@ elif section == "Interactive Injury Prediction Tool (BETA)":
 
 else:
     st.header("Conclusions and Future Work")
+
+st.write("Problems")
+st.write("")
+st.write("Data Scrapping")
+st.write("There was a considerable number of roadblocks in data scrapping, and we did not envision the challenges before we \
+    proceeded with the harvesting of data. The process of scrapping data from the web was a challenge for most of us because \
+    of multiple reasons. First of all, we were not convinced on what data to scrape for our models since we wanted to build a \
+    time series model. On top of that, we were looking at more than 3 sources at first. Second, there were many players available \
+    and a good number of attributes to choose from the websites. However, many of those attributes weren’t available for certain \
+    leagues or players, and when they were available, it seems that they were only obtainable for certain players. Also, those new \
+    features were not available for all the years, this is probably because this is new data that just started to being recorded \
+    recently. Third, scraping the data from the web required high computation and a great amount of memory capacity. We tried a few \
+    strategies to work around it. We attempted to use AWS, but we also faced many challenges with the installation as well as \
+    making the connection to the server; AWS preserved losing connection which resulted in errors in the middle of our data pulling \
+    process. On the other hand, we split the list of players to be scrapped in multiple batches between the three of us. This not only \
+    required time since we were running scripts overnight but also, required all of us to continue checking on the progress and \
+    communicating with each other to ensure the scrapping was working or finalized. Additionally, when we first completed scrapping \
+    the data from our sources, we realized that something was missed so we needed to go back to improve our API and repeat the same \
+    process more than one time. We even bumped into a few IP blocks after a high number of requests from the same IP address. In \
+    the end, we managed to scrape all the data that was required to build our models by working cooperatively.")
+st.write("")
+st.write("GitHub in Visual Studio Code")
+st.write("First, it took some effort to locally install and integrate VS Code and GitHub together. After the installation finished,\
+    and the repository was set and authenticated, we were capable to begin working in parallel, which we all enjoyed. However, we did \
+    undergo some minor problems when submitting new changes and pulling new requests. This mainly occurred because we failed to \
+    communicate which notebooks were being updated, as well as not pulling on time when needed to. This resulted in many conflicts \
+    that were not straightforward to solve, which even cause resetting the repositories in some cases. Once we were all accustomed to it, \
+    we did not face major problems. ")
+st.write("")
+st.write("Amazon Web Services")
+st.write("In addition to using VS Code to work with our GitHub repository, we incorporated Amazon Web Services so that we can attempt\
+    to run scripts that required a great amount of memory usage. The process of setting AWS was lengthy, and although we found guidelines\
+    on how to install and integrate AWS into our machines, it was still confusing to deal with it. Once AWS was installed and running, \
+    all of us started to experience problems with the connection during the web scrapping phase. This occurred regularly, thus we \
+    decided to avoid employing AWS for the data scrapping.")
+st.write("")
+st.write("")
+st.write("")
