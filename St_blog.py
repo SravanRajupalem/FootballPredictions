1ac651b72434b5a2ec328179a9575efb722184ef
dsvictorruiz <dsvictor@umich.edu>
2021-12-18 19:07:47 -0500

    test

--- St_blog.py
+++ St_blog.py
@@ -591,7 +591,7 @@ else:
 
     
     st.subheader("Conclusion")
-    st.write("<h1 style='text-align: center;'>In today’s football, players compete more and moreover a single year. Apart from playing more than one \
+    st.write("<h1 style='text-align: justify;'>In today’s football, players compete more and moreover a single year. Apart from playing more than one \
         tournament for their clubs, who own the rights for the players, the most talented players must attend \
         international duties with their national teams, as well as friendly matches with both their clubs and \
         countries. It is normal to see footballers get injured from time to time since this sport requires physical \
