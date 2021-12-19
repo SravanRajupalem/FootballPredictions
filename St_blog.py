b05c0c8483240414ecdead866a6db0a4d17defcd
SravanRajupalem <sravanr@umich.edu>
2021-12-19 11:46:04 +1100

    Center alignment

--- St_blog.py
+++ St_blog.py
@@ -330,7 +330,7 @@ elif section == "Model Building":
     
     st.header("Model Building")
     
-    st.write("""Now we are into the model building phase of the project. The first thing we need to do is to specify the target variables. In this case, \
+    st.write(<div style="text-align: center"> your-text-here </div> """Now we are into the model building phase of the project. The first thing we need to do is to specify the target variables. In this case, \
         we are looking at historic data of players to see when injuries occured and try to use that information to anticipate when injuries are likely to happen in the future.\
             This was done by creating the target variable, whether a player got injured or not, using five different time periods:
             \n- One Week
@@ -338,6 +338,8 @@ elif section == "Model Building":
             \n- One Quarter (3 months)
             \n- One Semester (6 months)
             \n- One Year (12 months)
+            
+            \n
             """)
 
 # SECTION: INJURY PREDICTION TOOL
