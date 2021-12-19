adb35116f28c08f2cd6ca21c538ee286230c3791
SravanRajupalem <sravanr@umich.edu>
2021-12-19 11:55:18 +1100

    Changes to blog

--- St_blog.py
+++ St_blog.py
@@ -332,7 +332,14 @@ elif section == "Model Building":
     st.header("Model Building")
     
     components.html("""
-                    <div style="text-align: center"> your-text-here </div>
+                    <div style="text-align: center"> Now we are into the model building phase of the project. The first thing we need to do is to specify the target variables. In this case, \
+        we are looking at historic data of players to see when injuries occured and try to use that information to anticipate when injuries are likely to happen in the future.\
+            This was done by creating the target variable, whether a player got injured or not, using five different time periods:
+            \n- One Week
+            \n- One Month
+            \n- One Quarter (3 months)
+            \n- One Semester (6 months)
+            \n- One Year (12 months) </div>
                     """)
     
     st.write("""Now we are into the model building phase of the project. The first thing we need to do is to specify the target variables. In this case, \
