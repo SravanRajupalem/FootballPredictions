2a496a6e22227d15669fa485a3797ef72fc8873b
oznerm <renzomaldonado@gmail.com>
2021-12-19 21:59:25 -0500

    proof reading changes

--- St_blog.py
+++ St_blog.py
@@ -208,7 +208,7 @@ elif section == "Data Manipulation":
     st.image(img13) 
     st.write("<p style='text-align: justify; font-size: 16px'>We believed competitions or tournaments where players participated could influence our model, especially when players are on international duty \
         to play for major tournaments such as the world qualifiers. If a football player gets injured due to international duty, this creates friction between the club and \
-        the player's national team, but we are not interested in that. We are more interested in understanding how these variables affect players' performance and how it makes them prone to injury. \
+        the player's national team. We are interested in understanding how these variables affect players' performance and how it makes them prone to injury. \
         Several questions arose. Are players more prone to get injured if they perform better? \
         Are players more likely to get hurt during a world cup qualifying game? Does the venue \
         of a game have an influence on a player's probability of injury?</h1>", unsafe_allow_html=True)
