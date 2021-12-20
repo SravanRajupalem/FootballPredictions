cb6121c6efb1a8ba6ab8005de2a7de8b4ec4dbed
oznerm <renzomaldonado@gmail.com>
2021-12-19 22:39:59 -0500

    proof reading changes

--- St_blog.py
+++ St_blog.py
@@ -568,7 +568,7 @@ set_config('y_test', y_test)
                 
                 \n<p style='text-align: justify; font-size: 16px'>The overall performance of the models reduces when we try to predict in longer time periods. Intuitively, this is because as the horizon gets longer \
                     there are more uncertainties and hence prediction is a more difficult task. There was some overfitting observed in the larger horizon models which was partially reduced by applying cross validation. \
-                        In future work, this be further could be reduced by training with more data, removing features that are not predictive in the long-term, early stopping and ensembling.
+                        In future work, this could be reduced by training with more data, removing features that are not predictive in the long-term, early stopping and ensembling.
                         
                         \n<p style='text-align: justify; font-size: 16px'> In the next section we will dive deeper into further tuning and evaluating the one-week model. 
             """, unsafe_allow_html=True)
